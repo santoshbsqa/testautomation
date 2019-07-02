615fad55e1130628ca2f1569102512f11c5c42e9
santi <santi@10.0.2.15>
2019-07-02 06:42:18 +0530

    test-sample

---
+++ src/test/java/testpack/TestScenario101.java
@@ -0,0 +1,15 @@
+package testpack;
+
+import org.testng.annotations.Test;
+
+public class TestScenario101 {
+	@Test
+	public void testCase101() {
+		System.out.println("hello from testCase101");
+	}
+
+	@Test
+	public void testCase102() {
+		System.out.println("hello from testCase102");
+	}
+}
